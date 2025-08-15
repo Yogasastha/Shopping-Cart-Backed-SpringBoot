d3be77d056c680d67dc6aee4479d6ad7ef227595
Yogasastha K <yogasasthak@gmail.com>
2025-08-15 11:35:19 +0530

    Debugging Error, when clear the lombok plugin's and maven clean and install.

--- src/main/java/com/upskilling/project/dreamshop/model/Category.java
+++ src/main/java/com/upskilling/project/dreamshop/model/Category.java
@@ -2,13 +2,13 @@ package com.upskilling.project.dreamshop.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
 
+
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Category {
@@ -17,8 +17,10 @@ public class Category {
     private Long id;
     private String name;
 
+
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
-    private List<Product> productList;
+    private List<Product> products;
 
     public Category(String name) {
         this.name = name;
--- src/main/java/com/upskilling/project/dreamshop/model/Image.java
+++ src/main/java/com/upskilling/project/dreamshop/model/Image.java
@@ -14,17 +14,15 @@ import java.sql.Blob;
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long  id;
+    private Long id;
     private String fileName;
     private String fileType;
-    private String downloadUrl;
 
-    // Multi-media type
     @Lob
     private Blob image;
+    private String downloadUrl;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
-
 }
